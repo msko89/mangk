f369dc0fc8a1236510709aefb445021804bafd8c
msko <msko@mcnc.co.kr>
2019-06-28 16:29:24 +0900

    text 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return <div className="App">TEST World</div>;
+    return <div className="App">MangK</div>;
 }
 
 export default App;
