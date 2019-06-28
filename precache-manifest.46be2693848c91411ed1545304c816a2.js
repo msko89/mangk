d923ccc1984fd29b02bb48dcb9e99eece999ba1e
msko <msko@mcnc.co.kr>
2019-06-28 16:10:11 +0900

    Updates

---
+++ precache-manifest.46be2693848c91411ed1545304c816a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1081603ab4b7b3dc01531e6f77fd35d",
+    "url": "/mangk/index.html"
+  },
+  {
+    "revision": "bfe8a67f4244de19940c",
+    "url": "/mangk/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/mangk/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bfe8a67f4244de19940c",
+    "url": "/mangk/static/js/main.a90f1324.chunk.js"
+  },
+  {
+    "revision": "133a23f1d33d3260f527",
+    "url": "/mangk/static/js/runtime~main.7d0122fb.js"
+  }
+]);
